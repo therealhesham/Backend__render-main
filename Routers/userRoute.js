1be287aa4361e041189a26c4c5d73d658e7891a4
therealhesham <68483818+therealhesham@users.noreply.github.com>
2024-01-03 15:55:02 +0200

    Update userRoute.js

--- Routers/userRoute.js
+++ Routers/userRoute.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const { add } = require("../controller/user")
 
 route.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://testfrontend-eta.vercel.app/');
     res.header({ "Access-Control-Allow-Credentials": true });
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
     res.header("Access-Control-Max-Age", 24 * 60 * 60 * 1000);

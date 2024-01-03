6f88883675b8aa99b3bc8cde311882d48273fa9e
thereakhesham <heshammoha231992@gmail.com>
2024-01-03 19:11:37 +0200

    commit on master

--- controller/user.js
+++ controller/user.js
@@ -83,23 +83,13 @@ console.log("sss")
       console.log(userData)
 const newuser =await userData.save()
       sendWelcomeEmail(newuser)
-      res.status(200).json()
+      res.send(newuser)
   }  
   next()    
   
   
   },async (req,res)=>{
 
-
-    res.header("Access-Control-Allow-Origin", "https://testfrontend-eta.vercel.app/");
-
-    res.header("Access-Control-Allow-Origin", "https://testfrontend-eta.vercel.app");
-    res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Max-Age", 24*60*60*1000);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  
-    
 
 }
 

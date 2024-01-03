99e3fb44c31a0ee0c49d3e500c207e0917182edb
thereakhesham <heshammoha231992@gmail.com>
2024-01-03 18:10:48 +0200

    commit on master

--- controller/user.js
+++ controller/user.js
@@ -70,48 +70,50 @@ console.log("sss")
       sameSite : "None",SameSite:"None" 
   }
   );
-  
+
+  const {role,firstName,lastName,email,phone} = req.body
+  if(!firstName){
+      res.status(400).json({message:"First Name is required"})
+  }
+  else if(!lastName){
+      res.status(400).json({message:"Last Name is required"})
+  }
+  else if(!email){
+      res.status(400).json({message:"Email is required"})
+  }
+  else if(!phone){
+      res.status(400).json({message:"Phone is required"})
+  }
+  else{
+      
+      const userData = await user.create({
+          role:role,
+          firstName:firstName,
+          lastName:lastName,
+          email:email,
+          phone:phone
+      })
+      console.log(userData)
+      sendWelcomeEmail(userData)
+      res.status(200).json()
+  }
+    
   
   next()    
   
   
   },async (req,res)=>{
 
+
+    res.header("Access-Control-Allow-Origin", "https://testfrontend-eta.vercel.app/");
+
     res.header("Access-Control-Allow-Origin", "https://testfrontend-eta.vercel.app");
-    
     res.header({"Access-Control-Allow-Credentials": true});
     res.header("Access-Control-Max-Age", 24*60*60*1000);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   
-console.log(req.body)    
-
-const {role,firstName,lastName,email,phone} = req.body
-if(!firstName){
-    res.status(400).json({message:"First Name is required"})
-}
-else if(!lastName){
-    res.status(400).json({message:"Last Name is required"})
-}
-else if(!email){
-    res.status(400).json({message:"Email is required"})
-}
-else if(!phone){
-    res.status(400).json({message:"Phone is required"})
-}
-else{
     
-    const userData = await user.create({
-        role:role,
-        firstName:firstName,
-        lastName:lastName,
-        email:email,
-        phone:phone
-    })
-    console.log(userData)
-    sendWelcomeEmail(userData)
-    res.status(200).json()
-}
 }
 
 )

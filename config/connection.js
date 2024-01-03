51215e33fae02fbc3da85ef1b1a7fd5c5a974141
thereakhesham <heshammoha231992@gmail.com>
2024-01-03 18:59:07 +0200

    commit on master

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const uri = "mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/?retryWrites=true&w=majority&ssl=true";
+const uri = "mongodb+srv://lvw:LVW123456789@cluster0.9bdwe65.mongodb.net/landingpage?retryWrites=true&w=majority&ssl=true";
 console.log('MongoDB URI:', uri);
 
 
